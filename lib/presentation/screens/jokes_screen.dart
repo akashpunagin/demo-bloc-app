4a6488f0195c5bbea38c0d46a2f8b9e8ba7017a4
akash <akash.punagin@gmail.com>
2021-10-30 13:04:32 +0530

    implemented state management for jokes screen

--- lib/presentation/screens/jokes_screen.dart
+++ lib/presentation/screens/jokes_screen.dart
@@ -1,4 +1,5 @@
 import 'package:bloc_demo/logic/blocs/jokes/jokes_bloc.dart';
+import 'package:bloc_demo/logic/cubits/internet/internet_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -11,63 +12,59 @@ class JokesScreen extends StatefulWidget {
 
 class _JokesScreenState extends State<JokesScreen> {
 
-  late JokesBloc jokesBloc;
 
   Widget getFetchJokesButton({required BuildContext context}) {
     return ElevatedButton(
+      style: TextButton.styleFrom(backgroundColor: Colors.blueGrey),
       onPressed: () {
-        jokesBloc.add(JokesFetched());
+        context.read<JokesBloc>().add(JokesFetched());
       },
       child: Text("GET JOKE"),
     );
   }
 
   @override
-  void initState() {
-    jokesBloc = BlocProvider.of<JokesBloc>(context);
-    jokesBloc.add(JokesFetched());
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("JOKES SCREEN"),
+        title: const Text("JOKES SCREEN"),
+        actions: [
+          getFetchJokesButton(context: context),
+        ],
       ),
-      body: BlocConsumer<JokesBloc, JokesState>(
-        bloc: context.watch<JokesBloc>(),
-        listener: (context, state) {
-          if (state is JokesFetchLoaded) {
-            ScaffoldMessenger.of(context).removeCurrentSnackBar(reason: SnackBarClosedReason.swipe);
-            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Joke Fetched"),));
-          }
-        },
-        builder: (context, state) {
-          print("STATE IS : $state");
-          if (state is JokesInitial) {
-            return getFetchJokesButton(context: context);
-          } else if (state is JokesFetchLoading) {
-            return CircularProgressIndicator();
-          } else if (state is JokesFetchError) {
-            return Column(
-              children: [
-                Text("OOPS! ${state.errorMessage}"),
-                getFetchJokesButton(context: context),
-              ],
-            );
-          } else if (state is JokesFetchLoaded) {
-            return Column(
-              children: [
-                Text("DETAILS: ${state.joke.toJson().toString()}"),
+      body: Builder(
+          builder: (context) {
+            final InternetState internetState = context.watch<InternetCubit>().state;
 
-                getFetchJokesButton(context: context),
-              ],
+            if (internetState is InternetDisconnected) {
+              return const Text("Please connect to internet");
+            }
+
+            return BlocConsumer<JokesBloc, JokesState>(
+              bloc: context.read<JokesBloc>(),
+              listener: (context, state) {
+                if (state is JokesFetchLoaded) {
+                  ScaffoldMessenger.of(context).removeCurrentSnackBar(
+                      reason: SnackBarClosedReason.swipe);
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text("Joke Fetched"),));
+                }
+              },
+              builder: (context, state) {
+                if (state is JokesInitial) {
+                  return getFetchJokesButton(context: context);
+                } else if (state is JokesFetchLoading) {
+                  return const CircularProgressIndicator();
+                } else if (state is JokesFetchError) {
+                  return Text("OOPS! ${state.errorMessage}");
+                } else if (state is JokesFetchLoaded) {
+                  return Text("DETAILS: ${state.joke.toJson().toString()}");
+                } else {
+                  return const CircularProgressIndicator();
+                }
+              },
             );
-          } else {
-            return CircularProgressIndicator();
           }
-        },
       ),
     );
   }
